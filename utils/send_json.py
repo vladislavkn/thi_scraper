ebe00d674bf885a15965c3d148fc886684fefd5b
Vladislav Knyshov <inova136@gmail.com>
2023-11-02 10:14:18 +0100

    fix: data sending

--- utils/send_json.py
+++ utils/send_json.py
@@ -3,7 +3,12 @@ import requests
 
 def send_json(data, url):
     headers = {"Content-Type": "application/json"}
-    response = requests.post(url, json=data, headers=headers)
+
+    response = requests.post(
+        url,
+        data=data,
+        headers=headers,
+    )
 
     if response.status_code != 200:
         raise Exception("Reuest failed with status code " + response.status_code)
