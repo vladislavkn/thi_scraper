e98f373d8addadf38f511ee32206c29c045e0758
Vladislav Knyshov <inova136@gmail.com>
2023-11-02 10:03:43 +0100

    fix: server url

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from utils.get_spider_results_json import get_spider_results_json
 if __name__ == "__main__":
     current_directory = os.path.dirname(os.path.realpath(__file__))
     result_filename = os.path.join(current_directory, "results.json")
+    url = "http://10.10.240.11:5005/api/answers/"
 
     print("Starting spider...")
     run_spider(WikiSpider, result_filename)
@@ -15,7 +16,7 @@ if __name__ == "__main__":
 
     print("Sending results...")
     try:
-        send_json(spider_results, 5000)
+        send_json(spider_results, url)
         print("Data sent successful")
     except Exception as exception:
         print(exception)
--- utils/send_json.py
+++ utils/send_json.py
@@ -1,9 +1,9 @@
 import requests
 
 
-def send_json(data, port):
+def send_json(data, url):
     headers = {"Content-Type": "application/json"}
-    response = requests.post(f"http://localhost:{port}", json=data, headers=headers)
+    response = requests.post(url, json=data, headers=headers)
 
     if response.status_code != 200:
         raise Exception("Reuest failed with status code " + response.status_code)
